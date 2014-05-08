0c75b8a90a887e2af8f5ed41434dfebffdb085d6
magnushg <magnus.h.green@gmail.com>
2014-05-08 22:14:20 +0200

    Added some styling to proximity, removed more public transport related code

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -6,40 +6,12 @@
         $scope.proximity = automatrService.getProximity();
         $scope.lightswitchToggle = automatrService.lightswitch();
         $scope.environmentLog = automatrService.environmentLog();
-        $scope.monitoredStop = automatrService.monitoredStop();
         $scope.lightswitchToggle.$bind($scope, 'lightToggle');
-        $scope.selectedDeparture = -1;
         $scope.temperatureChartConfig = { loading: true };
         $scope.brightnessChartConfig = { loading: true };
 
-        $scope.selectDeparture = function($index, $item) {
-            $scope.selectedDeparture = $index;
-            travelService.setSubscription($scope.monitoredStop, $scope.selectedStop, $item);
-        };
-
         $scope.lightswitch = function() {            
             $scope.lightToggle = !$scope.lightToggle;
-        };
-
-        $scope.getLocation = function(val) {
-            return travelService.getLocation(val).then(function(res) {
-                return travelService.mapStops(res.data);
-            });
-        };
-
-         $scope.stopSelected = function($item) {
-            $scope.selectedDeparture = -1;
-            $scope.selectedStop = $item;
-            return travelService.getRealtimeData($item.id).then(function(departures) {
-                    if(departures.data.length > 0 ) {
-                        $scope.departuresHeading = 'Real time departures';
-                        $scope.departures = travelService.mapDepartures(departures);
-                    }
-                    else {
-                        $scope.departuresHeading = 'No Real time data';
-                        $scope.departures = {};
-                    }
-                });
         };
 
         $scope.temperature.$on('loaded', function (data) {
