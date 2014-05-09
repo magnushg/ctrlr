45341c574c1b3e3a3e879b11721d306c9b106a95
magnushg <magnus.h.green@gmail.com>
2014-05-09 12:30:31 +0200

    Moved lightswitch

--- app/services/automatrService.js
+++ app/services/automatrService.js
@@ -2,14 +2,14 @@
 
     var automatrService = function ($firebase) {
         var automatrFactory = {};
-        var firebaseName = "automatr";
+        var firebaseName = "automatr-test";
 
         var lightswitchRef = new Firebase("https://{0}.firebaseio.com/lightswitch".format(firebaseName));
         var temperatureRef = new Firebase("https://{0}.firebaseio.com/temperature".format(firebaseName));
         var proximityRef = new Firebase("https://{0}.firebaseio.com/proximity".format(firebaseName));
         var brightnessRef = new Firebase("https://{0}.firebaseio.com/brightness".format(firebaseName));
         var logRef = new Firebase("https://{0}.firebaseio.com/environmentLog".format(firebaseName));
-        var monitoredStopRef = new Firebase("https://{0}.firebaseio.com/monitoredStop".format(firebaseName));
+        var monitoredStopRef = new Firebase("https://{0}.firebaseio.com/monitoredStop".format(firebaseName)); 
         
         automatrFactory.gettemperature = function () {
         	return $firebase(temperatureRef);
